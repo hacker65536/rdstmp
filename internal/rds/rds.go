3aa21e0e444da20eef55becdb3084dd812daa289
hacker65536 <s.hacker65536@gmail.com>
2020-06-15 16:25:27 +0000

    add files

--- internal/rds/rds.go
+++ internal/rds/rds.go
@@ -1,15 +1,42 @@
-package rds
+package rdsfunc
 
 import (
-	"github.com/aws/aws-sdk-go/service/rds"
-	"github.com/hacker65536/rds/internal/aws"
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/rds"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 var (
 	rdssvc *rds.Client
+
+	awsCfg aws.Config
 )
 
 func init() {
-	cfgCp := cfg.Copy()
-	rdssvc = rds.New(cfgCp)
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		panic("unable to load SDK config, " + err.Error())
+	}
+	rdssvc = rds.New(cfg)
+
+}
+
+func getrds(filter string) {
+	params := &rds.DescribeDBInstancesInput{}
+	req := rdssvc.DescribeDBInstancesRequest(params)
+
+	p := rds.NewDescribeDBInstancesPaginator(req)
+
+	for p.Next(context.TODO()) {
+		page := p.CurrentPage()
+
+		for _, v := range page.DBInstances {
+			fmt.Println(aws.StringValue(v.DBInstanceIdentifier))
+		}
+
+	}
+
 }
